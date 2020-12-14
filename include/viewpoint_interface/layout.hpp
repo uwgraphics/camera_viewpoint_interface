dbab9a31dee8426541506213bae650a9f86624f2
Luis Molina <lamolina@wisc.edu>
2020-12-13 19:38:55 -0600

    Created LayoutManager class to handle switching and caching layouts.

--- include/viewpoint_interface/layout.hpp
+++ include/viewpoint_interface/layout.hpp
@@ -6,6 +6,8 @@
 namespace viewpoint_interface
 {
 
+    class LayoutManager;
+
     enum LayoutType
     {
         NONE = -1,
@@ -22,24 +24,34 @@ namespace viewpoint_interface
 
     class Layout
     {
-    public:        
-        const std::string& getLayoutName(LayoutType type) const
-        {
-            return layout_names[(uint)type];
+    public:
+        LayoutType getLayoutType() const { return layout_type; } 
+
+        const std::vector<std::string> getLayoutList() const 
+        { 
+            return layout_names; 
         }
+
+        const std::string getLayoutName() const
+        {
+            if (layout_type == LayoutType::NONE) {
+                return "Layouts Inactive";
+            }
+
+            return layout_names[(uint)layout_type];
+        }
+
 
     protected:
         Layout(LayoutType type) : layout_type(type) {}
 
     private:
-        static const uint num_layout_types = 6;
-        const std::string layout_names[num_layout_types] = {
+        const std::vector<std::string> layout_names = {
             "Movie", "Grid", "Split Screen", "Twinned",
             "Picture-in-Picture", "Carousel"
         };
 
         LayoutType layout_type;
-
     };
     
 
@@ -47,12 +59,6 @@ namespace viewpoint_interface
     {
     public:
         NoneLayout() : Layout(LayoutType::NONE) {}
-
-        const std::string getLayoutName(LayoutType type) const
-        {
-            return "Invalid layout";
-        }
-
     };
 
     class MovieLayout : public Layout
@@ -91,6 +97,130 @@ namespace viewpoint_interface
         CarouselLayout() : Layout(LayoutType::CAROUSEL) {}
     };
 
+
+    class LayoutManager
+    {
+    public:
+        LayoutManager() : active_layout(new NoneLayout()) {}
+
+        static LayoutType intToLayoutType(int ix) {
+            if (ix >= 0 && ix < num_layout_types) {
+                return (LayoutType)ix;
+            }
+
+            return LayoutType::NONE;
+        }
+
+        const std::vector<std::string> getLayoutList() const 
+        { 
+            return active_layout->getLayoutList(); 
+        }
+
+        std::shared_ptr<Layout> getActiveLayout() const
+        {
+            return active_layout;
+        }
+
+        bool isLayoutActive(LayoutType type) const
+        {
+            if (active_layout->getLayoutType() == type) {
+                return true;
+            }
+
+            return false;
+        }
+
+        void activateLayout(LayoutType type)
+        {
+            // Already active
+            if (active_layout->getLayoutType() == type) {
+                return;
+            }
+
+            // We cache previously active layouts so that their params are not reset
+            if (!isInCache(active_layout->getLayoutType())) {
+                layouts_cache.push_back(active_layout);
+            }
+
+            if (isInCache(type)) {
+                active_layout = getLayoutFromCache(type);
+            }
+            else {
+                active_layout = newLayout(type);
+            }
+        }
+
+    private:
+        static const uint num_layout_types = 6;
+
+        std::vector<std::shared_ptr<Layout>> layouts_cache;
+        std::shared_ptr<Layout> active_layout;
+
+        std::shared_ptr<Layout> newLayout(LayoutType type)
+        {
+            std::shared_ptr<Layout> layout;
+            switch(type) {
+                case LayoutType::MOVIE:
+                {
+                    layout = std::shared_ptr<Layout>(new MovieLayout());
+                } break;
+
+                case LayoutType::GRID:
+                {
+                    layout = std::shared_ptr<Layout>(new GridLayout());
+                } break;
+
+                case LayoutType::SPLIT:
+                {
+                    layout = std::shared_ptr<Layout>(new SplitLayout());
+                } break;
+
+                case LayoutType::TWINNED:
+                {
+                    layout = std::shared_ptr<Layout>(new TwinnedLayout());
+                } break;
+
+                case LayoutType::PIP:
+                {
+                    layout = std::shared_ptr<Layout>(new PipLayout());
+                } break;
+
+                case LayoutType::CAROUSEL:
+                {
+                    layout = std::shared_ptr<Layout>(new CarouselLayout());
+                } break;
+
+                default: 
+                {
+                    layout = std::shared_ptr<Layout>(new NoneLayout());
+                } break;
+            }
+            
+            return layout;
+        }
+
+        bool isInCache(LayoutType type) const
+        {
+            for (const std::shared_ptr<Layout> layout : layouts_cache) {
+                if (layout->getLayoutType() == type) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        std::shared_ptr<Layout> getLayoutFromCache(LayoutType type) const
+        {
+            for (const std::shared_ptr<Layout> layout : layouts_cache) {
+                if (layout->getLayoutType() == type) {
+                    return layout;
+                }
+            }
+
+            return std::shared_ptr<Layout>(new NoneLayout());
+        }
+    };
 
 } // viewpoint_interface
 
