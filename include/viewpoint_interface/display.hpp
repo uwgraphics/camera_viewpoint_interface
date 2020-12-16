568de0d38f6cd873d8b266b03a4277f3bf4023e0
Luis Molina <lamolina@wisc.edu>
2020-12-15 18:01:01 -0600

    Started making changes to support interaction with the LayoutManager.

--- include/viewpoint_interface/display.hpp
+++ include/viewpoint_interface/display.hpp
@@ -39,7 +39,7 @@ namespace viewpoint_interface
     public:
 
         Display(std::string &internal, std::string &external, std::string &topic, const DisplayDims &dims) :
-                int_name(internal), ext_name(external), topic_name(topic), dimensions(dims) {}
+                int_name(internal), ext_name(external), topic_name(topic), dimensions(dims), active(true) {}
 
         inline std::string getInternalName() const { return int_name; }
         inline std::string getExternalName() const { return ext_name; }
@@ -47,22 +47,41 @@ namespace viewpoint_interface
 
         const DisplayDims& dims() const { return dimensions; }
 
+        bool isActive() const { return active; }
+        void activate() { active = true; }
+        void deactivate() { active = false; }
+        void flipState() { active = !active; }
+
     private:
         std::string int_name, ext_name, topic_name;
         DisplayDims dimensions;
+        bool active;
     };
 
+
+    enum class DisplayLoadHint
+    {
+        DisplayHint_LoadOne,    // Load only one display--unable to predict next one
+        DisplayHint_Neighbors   // Load previous and next displays in ring
+    };
 
     class DisplayManager
     {
     public:
         static const uint max_buffers = 9;
 
-        DisplayManager(uint num = max_buffers, DisplayDims dims = DisplayDims{1024, 1024, 3})
+        DisplayManager(uint num=max_buffers, DisplayDims dims=DisplayDims{1024, 1024, 3})
         {
             for (int i = 0; i < num; i++) {
                 buffers.push_back(std::vector<uint>(dims.size()));
             }
+        }
+
+        uint size() const { return displays.size(); }
+
+        Display &operator [](uint ix)
+        {
+            return displays.at(ix);
         }
 
         void addDisplay(const Display &disp)
@@ -70,17 +89,25 @@ namespace viewpoint_interface
             displays.push_back(disp);
         }
 
-        uint size() { return displays.size(); }
-
-        Display operator [](uint ix)
+        void bufferDisplay()
         {
-            return displays.at(ix);
+            // TODO: Allow manually buffering
         }
 
     private:
         std::vector<Display> displays;
         std::vector<uint> active_buffers;
         std::vector<std::vector<uint>> buffers;
+
+        uint numBuffersUsed() const
+        {
+            return active_buffers.size();
+        }
+
+        bool isBufferAvailable() const
+        {
+            return numBuffersUsed() < max_buffers;
+        }
     };
 
 } // viewpoint_interface
